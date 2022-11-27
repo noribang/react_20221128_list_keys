841a5af33b4fd4f6bf49cb4d5dac5713d40c4b93
hoonkim <hoonk9387@gmail.com>
2022-11-27 09:52:20 -0600

    set-up for list and keys.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import EmojiButton from './EmojiButton'
+import Header from './Header'
+import PetCard from './PetCard'
+
+const pets = [
+  { id: 1, name: "Fezzik", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/23124207/Old-English-Sheepdog-running-in-the-park.jpg" },
+  { id: 2, name: "Lucy", image: "https://emborapets.com/wp-content/uploads/2020/08/Why-Does-My-Springer-Spaniel-Smell-So-Bad_.jpg"},
+  { id: 3, name: "Blaise", image: "https://www.thesprucepets.com/thmb/P8d4QM0bI4rJh1WygZDI8f8kNV4=/1000x1000/smart/filters:no_upscale()/GettyImages-870255568-8639765b08c6441dbe35ed72e2d8fb48.jpg" }
+]
 
 function App() {
+  
+  const petCards = pets.map((petObj) => {
+    return <PetCard name={petObj.name} image={petObj.image} />
+  })
+  
+  console.log(petCards)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main>
+        {petCards}
+      </main>
     </div>
   );
 }
 
-export default App;
+export default App
