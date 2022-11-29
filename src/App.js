541ba97f9b26db11355381c77f447e9d65edd11b
hoonkim <hoonk9387@gmail.com>
2022-11-28 20:58:16 -0600

    Call map method on array.

--- src/App.js
+++ src/App.js
@@ -3,28 +3,53 @@ import EmojiButton from './EmojiButton'
 import Header from './Header'
 import PetCard from './PetCard'
 
+// const pets = [
+//   { id: 1, name: "Fezzik", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/23124207/Old-English-Sheepdog-running-in-the-park.jpg" },
+//   { id: 2, name: "Lucy", image: "https://emborapets.com/wp-content/uploads/2020/08/Why-Does-My-Springer-Spaniel-Smell-So-Bad_.jpg"},
+//   { id: 3, name: "Blaise", image: "https://www.thesprucepets.com/thmb/P8d4QM0bI4rJh1WygZDI8f8kNV4=/1000x1000/smart/filters:no_upscale()/GettyImages-870255568-8639765b08c6441dbe35ed72e2d8fb48.jpg" }
+// ]
 const pets = [
-  { id: 1, name: "Fezzik", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/23124207/Old-English-Sheepdog-running-in-the-park.jpg" },
-  { id: 2, name: "Lucy", image: "https://emborapets.com/wp-content/uploads/2020/08/Why-Does-My-Springer-Spaniel-Smell-So-Bad_.jpg"},
-  { id: 3, name: "Blaise", image: "https://www.thesprucepets.com/thmb/P8d4QM0bI4rJh1WygZDI8f8kNV4=/1000x1000/smart/filters:no_upscale()/GettyImages-870255568-8639765b08c6441dbe35ed72e2d8fb48.jpg" }
+  { id: 1, name: "Fezzik", image: "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/23124207/Old-English-Sheepdog-running-in-the-park.jpg", favSnacks: ["Peanut Butter", "Soccer Balls"] },
+  { id: 2, name: "Lucy", image: "https://emborapets.com/wp-content/uploads/2020/08/Why-Does-My-Springer-Spaniel-Smell-So-Bad_.jpg", favSnacks: ["Cake", "Bacon"]},
+  { id: 3, name: "Blaise", image: "https://www.thesprucepets.com/thmb/P8d4QM0bI4rJh1WygZDI8f8kNV4=/1000x1000/smart/filters:no_upscale()/GettyImages-870255568-8639765b08c6441dbe35ed72e2d8fb48.jpg", favSnacks: ["Pigeons", "Grass", "Chicken"] }
 ]
 
 function App() {
-  // console.log(pets)
+  // Loop through object in array.
+  const petCards = pets.map(() => {})
 
-  const petCards = pets.map((petObj) => {
-    return <PetCard key={petObj.id} name={petObj.name} image={petObj.image}/>
-  })
-
-  console.log(petCards)
   return (
     <div>
       <Header />
       <main>
-          {petCards}
+        {petcards}
       </main>
     </div>
-  );
+  )
+
 }
+
+// function App() {
+//   // console.log(pets)
+
+//   const petCards = pets.map((petObj) => {
+//     return <PetCard 
+//             key={petObj.id} 
+//             name={petObj.name} 
+//             image={petObj.image} 
+//             favSnacks={petObj.favSnacks}
+//             />
+//   })
+
+//   // console.log(petCards)
+//   return (
+//     <div>
+//       <Header />
+//       <main>
+//           {petCards}
+//       </main>
+//     </div>
+//   );
+// }
 
 export default App
--- src/PetCard.js
+++ src/PetCard.js
@@ -1,12 +1,41 @@
 import React from 'react'
 
+// function PetCard(props) {
+//     console.log(props.favSnacks)
+
+//     // const snackList = props.favSnacks.map(snack => 
+//     //     <li key={snack}>{snack}</li>
+//     // )
+
+//   return (
+//     <div className="card">
+//       <h2>{props.name}</h2>
+//       <img src={props.image} alt={props.name} />
+//       <h3>Favorite Snacks</h3>
+//       <ul>
+//         {/* {snackList} */}
+//         {props.favSnacks.map(snack => <li key={snack}>{snack}</li>)}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// render a <li> for each element in the favSnacks array
+// remember to use a unique key prop
+
 function PetCard(props) {
+  console.log(props)
   return (
     <div className="card">
       <h2>{props.name}</h2>
       <img src={props.image} alt={props.name} />
+      <h3>Favorite Snacks</h3>
+      <ul>
+      {/* render snacks here */}
+      </ul>
     </div>
   )
 }
+
 
 export default PetCard
