9a66851349b6a4d22ff8f5642c7f70b6ced9dd03
hoonkim <hoonk9387@gmail.com>
2022-11-29 22:15:58 -0600

    Destructure props in petcards component.

--- src/PetCard.js
+++ src/PetCard.js
@@ -24,18 +24,27 @@ import EmojiButton from './EmojiButton';
 // render a <li> for each element in the favSnacks array
 // remember to use a unique key prop
 
-function PetCard(props) {
+// function PetCard(props) {
+function PetCard({ 
+    name, 
+    image="https://emborapets.com/wp-content/uploads/2020/08/Why-Does-My-Springer-Spaniel-Smell-So-Bad_.jpg", 
+    favSnacks 
+  }) {  
   // console.log(props.favSnacks)
 
-  const snackList = props.favSnacks.map((snack) => 
+  const snackList = favSnacks.map((snack) => 
     // console.log(snack)
     <li key={snack}>{snack}</li>
   )
+  // const snackList = props.favSnacks.map((snack) => 
+  //   // console.log(snack)
+  //   <li key={snack}>{snack}</li>
+  // )
 
   return (
     <div className="card">
-      <h2>{props.name}</h2>
-      <img src={props.image} alt={props.name} />
+      <h2>{name}</h2>
+      <img src={image} alt={name} />
       {/* <EmojiButton emoji="&#128021;" label="Adopt"/> */}
       <EmojiButton emoji="&#128018;" label="Save"/>
       <h3>Favorite Snacks</h3>
@@ -45,6 +54,20 @@ function PetCard(props) {
       </ul>
     </div>
   )
+
+  // return (
+  //   <div className="card">
+  //     <h2>{props.name}</h2>
+  //     <img src={props.image} alt={props.name} />
+  //     {/* <EmojiButton emoji="&#128021;" label="Adopt"/> */}
+  //     <EmojiButton emoji="&#128018;" label="Save"/>
+  //     <h3>Favorite Snacks</h3>
+  //     <ul>
+  //     {/* render snacks here */}
+  //     {snackList}
+  //     </ul>
+  //   </div>
+  // )
 }
 
 
